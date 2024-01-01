a410f354fc868599141e9b08ee0d3f770d0cb5b5
ianmaish <ianmaish607@gmail.com>
2023-12-31 16:50:33 -0800

    Final changes

--- W1D3/skeleton/lib/board.rb
+++ W1D3/skeleton/lib/board.rb
@@ -25,10 +25,35 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+
+    cup_idx = start_pos
+    until stones.empty? 
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else 
+        @cups[cup_idx] << stones.pop
+      end 
+    end 
+    render
+    next_turn(cup_idx)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else 
+      ending_cup_idx
+    end 
+
   end
 
   def render
@@ -40,8 +65,18 @@ class Board
   end
 
   def one_side_empty?
+    @cups[1...6].all? {|cup| cup.empty?} ||
+    @cups[7...12].all? {|cup| cup.empty?}
   end
 
   def winner
+    player1_count = @cups[6].count 
+    player2_count = @cups[13].count
+
+    if player1_count == player2_count
+      :draw
+    else 
+      player1_count > player2_count ? @name1 : @name2
+    end 
   end
 end
